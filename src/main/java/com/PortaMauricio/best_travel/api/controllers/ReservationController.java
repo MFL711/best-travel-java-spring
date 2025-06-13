eb33079665c52145de50370ed03c30d0ef2e1cb6
Mauricio Fernandez <mfernandezl@outlook.com>
2025-06-12 18:01:32 -0600

    Manejo de errores y validaciones

--- src/main/java/com/PortaMauricio/best_travel/api/controllers/ReservationController.java
+++ src/main/java/com/PortaMauricio/best_travel/api/controllers/ReservationController.java
@@ -3,6 +3,7 @@ package com.PortaMauricio.best_travel.api.controllers;
 import com.PortaMauricio.best_travel.api.models.request.ReservationRequest;
 import com.PortaMauricio.best_travel.api.models.responses.ReservationResponse;
 import com.PortaMauricio.best_travel.infraestructure.abstract_service.IReservationService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class ReservationController {
     private final IReservationService reservationService;
 
     @PostMapping
-    public ResponseEntity<ReservationResponse> post (@RequestBody ReservationRequest request){
+    public ResponseEntity<ReservationResponse> post (@Valid @RequestBody ReservationRequest request){
         return ResponseEntity.ok(reservationService.Create(request));
     }
 
@@ -30,7 +31,8 @@ public class ReservationController {
     }
 
     @PutMapping (path = "{id}")
-    public ResponseEntity<ReservationResponse> update (@RequestBody ReservationRequest request, @PathVariable UUID id){
+    public ResponseEntity<ReservationResponse> update (@Valid @RequestBody ReservationRequest request,
+                                                       @PathVariable UUID id){
         return ResponseEntity.ok(reservationService.Update(request,id));
     }
 
--- src/main/java/com/PortaMauricio/best_travel/api/models/request/ReservationRequest.java
+++ src/main/java/com/PortaMauricio/best_travel/api/models/request/ReservationRequest.java
@@ -1,13 +1,12 @@
 package com.PortaMauricio.best_travel.api.models.request;
 
-import com.PortaMauricio.best_travel.domain.entities.CustomerEntity;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,7 +14,21 @@ import java.util.UUID;
 @Builder
 public class ReservationRequest implements Serializable {
 
+    @Size(min = 18, max = 20, message = "The size have to a length between  18 and 30 characteres")
+    @NotBlank(message = "Id client is mandatory")
     private String customerId;
+    @Positive
+    @NotNull (message = "Id hotel is mandatory")
     private Long hotelId;
+    @Min(value = 1, message = "Min 1 day to make reservation")
+    @Max(value = 30, message = "Max 30 days to make reservation")
+    @NotNull (message = "Total days is mandatory")
     private Integer totalDays;
+    //@Pattern(regexp = "^(.+)@(.+)$") Aqui se utiliza una expresion o un patron de como debe de estar
+    // escrito nuesto email, sin embargo, jakarta ya tiene su propia notation que nos permite tener una
+    // validación más precisa.
+    @Email
+    private String email;
+
+    //Agregando validaciones
 }
